e499b7cbf794ee3cfeb5e19c03dcc77322e6502f
Shriya <svishwas1@hwemail.com>
2026-05-03 15:52:11 -0700

    Work Done on 05/03/26

---
+++ AP Review/APReview.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class APReview {
+    
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void testString() {
+        System.out.print("Give me a name: ");
+        String name = scanner.nextLine();
+        System.out.println(name);
+    }
+
+    public static void testInteger() {
+    }
+
+    
+}
---
+++ AP Review/APReviewTester.java
@@ -0,0 +1,5 @@
+public class APReviewTester {
+    public static void main (String[] args) {
+        APReview.testString();
+    }
+}
